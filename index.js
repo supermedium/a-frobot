a6eec031b2385a72dff3741d7a9ea7ef5f68df4e
Kevin Ngo <me@ngokevin.com>
2016-12-22 00:25:53 -0800

    body parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var app = express()
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.send('AFRO')
+  res.send('AFRO');
 })
 
 app.post('/postreceive', function (req, res) {
@@ -15,4 +18,4 @@ app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 })
 
-function bumpAframeDist (data) {}
+function bumpAframeDist (data) { }
