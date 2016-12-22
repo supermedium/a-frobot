b7fe6c17f38611efac1a8fd7af91d02a976fc0ca
Kevin Ngo <me@ngokevin.com>
2016-12-22 00:12:18 -0800

    express template

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+  res.send('AFRO')
+})
+
 app.post('/postreceive', function (req, res) {
   console.log(res);
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 })
