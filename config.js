4d43ed24e5863c49b92d6cb8f01bbd3c10bc2df1
Kevin <me@ngokevin.com>
2016-12-30 12:32:20 -0800

    fix staging config

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ let config = {
   userName: 'a-frobot'
 };
 
-if (process.env.AFROBOT_ENV !== 'staging') {
+if (process.env.AFROBOT_ENV === 'staging') {
   config = Object.assign(config, stagingConfig);
 }
 
